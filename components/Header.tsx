7a4f6643a88555065eb67d40f9eda389a4fc3445
Stitchal <alexis.rosset@etu.unice.fr>
2026-08-13 21:51:34 +0200

    remove sticky nav

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import NavBar from "@/components/NavBar";
 
 export default function Header() {
   return (
-    <header className="sticky top-0 z-50">
+    <header>
       <NavBar variant="page" />
     </header>
   );
