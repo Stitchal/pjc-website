84fabcc42a00233faeee94a857117ff100fa6137
Stitchal <alexis.rosset@etu.unice.fr>
2026-08-24 16:34:34 +0200

    enhanced navbar

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import NavBar from "@/components/NavBar";
 
 export default function Header() {
   return (
-    <header>
+    <header className="bg-white shadow-[0_1px_0_0_rgba(0,0,0,0.06)]">
       <NavBar variant="page" />
     </header>
   );
