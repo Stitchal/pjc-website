5bb112ad410ccf1f95393904c00fb65125e54b4a
Stitchal <alexis.rosset@etu.unice.fr>
2026-08-25 17:30:04 +0200

    enhanced footer

---
+++ lib/nav-links.ts
@@ -0,0 +1,5 @@
+export const NAV_LINKS = [
+  { href: "/", label: "Accueil" },
+  { href: "/notre-structure", label: "Notre structure" },
+  { href: "/nos-prestations", label: "Nos prestations" },
+] as const;
