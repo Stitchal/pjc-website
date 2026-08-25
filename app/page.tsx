057415348b9097b07ad9c0a263727b92e311d5ec
Stitchal <alexis.rosset@etu.unice.fr>
2026-08-25 16:59:41 +0200

    deleted CtaBanner import

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import HeroSection from "@/components/home/HeroSection";
 import IntroCard from "@/components/home/IntroCard";
 import StudyProcessSection from "@/components/home/StudyProcessSection";
 import ServicesGrid from "@/components/home/ServicesGrid";
-import CtaBanner from "@/components/home/CtaBanner";
 import PartnersSection from "@/components/home/PartnersSection";
 import CtaStrip from "@/components/CtaStrip";
 
